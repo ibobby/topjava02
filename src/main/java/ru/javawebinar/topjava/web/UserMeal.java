867c4f0e7002a5ed4037d10a259a1754e53e241d
bobby <ibobby0386@gmail.com>
2015-03-03 22:39:38 +0300

    Meal added

---
+++ src/main/java/ru/javawebinar/topjava/web/UserMeal.java
@@ -0,0 +1,20 @@
+package ru.javawebinar.topjava.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by bobby on 01.03.2015.
+ */
+public class UserMeal extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.sendRedirect("userMeal.jsp");
+    }
+}
---
+++ src/main/webapp/userMeal.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: bobby
+  Date: 01.03.2015
+  Time: 15:26
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>user meal</title>
+</head>
+<body>
+    <h2>What does he eat? <a href="index.html">На главную</a></h2>
+</body>
+</html>
