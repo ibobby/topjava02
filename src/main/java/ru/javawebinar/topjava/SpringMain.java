82d515428b5a7a30fcba8083f3dfc0539ba99bc6
gkislin@yandex.ru <github13>
2015-03-06 23:06:41 +0300

    Add Spring context and Main

---
+++ src/main/java/ru/javawebinar/topjava/SpringMain.java
@@ -0,0 +1,22 @@
+package ru.javawebinar.topjava;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ru.javawebinar.topjava.repository.mock.MockUserRepository;
+
+import java.util.Arrays;
+
+/**
+ * User: gkislin
+ * Date: 22.08.2014
+ */
+public class SpringMain {
+    public static void main(String[] args) {
+        ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml");
+        System.out.println(Arrays.toString(appCtx.getBeanDefinitionNames()));
+        MockUserRepository mockUserRepository = (MockUserRepository) appCtx.getBean("mockUserRepository");
+        mockUserRepository = appCtx.getBean(MockUserRepository.class);
+
+        appCtx.close();
+    }
+}
