23a3fb9c9debbf70234ac6ac7a5e455db6943670
ibobby <ibobby0386@gmail.com>
2015-03-15 20:13:54 +0300

    tuning

--- src/main/java/ru/javawebinar/topjava/model/UserMeal.java
+++ src/main/java/ru/javawebinar/topjava/model/UserMeal.java
@@ -1,24 +1,29 @@
 package ru.javawebinar.topjava.model;
 
+import java.time.LocalDateTime;
+
 /**
  * GKislin
  * 06.03.2015.
  */
-public class UserMeal extends NamedEntity {
+public class UserMeal extends BaseEntity {
 
     private User user;
+
+    protected String description;
+
+    protected int calories;
+
+    protected LocalDateTime dateTime;
 
     public UserMeal() {}
 
     public UserMeal(String name, User user) {
-
+        super();
     }
 
     @Override
     public String toString() {
-        return "UserMeal{" +
-                "name='" + name + '\'' +
-                "user=" + user +
-                '}';
+        return "Meal(" + id + ", " + dateTime + ", " + description + '\'' + "calories: " + calories +')';
     }
 }
