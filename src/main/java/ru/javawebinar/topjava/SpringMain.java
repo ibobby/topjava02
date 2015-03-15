f039e34d7131fc9542e89fae8b2cbbdf915f1cd6
bobby <ibobby0386@gmail.com>
2015-03-15 20:16:11 +0300

    tuning

--- src/main/java/ru/javawebinar/topjava/SpringMain.java
+++ src/main/java/ru/javawebinar/topjava/SpringMain.java
@@ -2,9 +2,8 @@ package ru.javawebinar.topjava;
 
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import ru.javawebinar.topjava.web.user.AdminUserRestController;
-
-import java.util.Arrays;
+import ru.javawebinar.topjava.model.UserMeal;
+import ru.javawebinar.topjava.web.meal.UserMealRestController;
 
 /**
  * User: gkislin
@@ -14,15 +13,15 @@ public class SpringMain {
     public static void main(String[] args) {
         // java 7 Automatic resource management
         try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml")) {
-            System.out.println("\n" + Arrays.toString(appCtx.getBeanDefinitionNames()) + "\n");
+           /* System.out.println("\n" + Arrays.toString(appCtx.getBeanDefinitionNames()) + "\n");
             AdminUserRestController adminController = appCtx.getBean(AdminUserRestController.class);
             adminController.delete(7);
-            adminController.getByMail("dummy");
+            adminController.getByMail("dummy");*/
 
-            /*UserMealRestController userMealController = appCtx.getBean(UserMealRestController.class);
+            UserMealRestController userMealController = appCtx.getBean(UserMealRestController.class);
             userMealController.create(new UserMeal());
             userMealController.delete(555);
-            userMealController.get(555);*/
+            userMealController.get(555);
         }
     }
 }
